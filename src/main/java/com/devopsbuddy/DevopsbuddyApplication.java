33206af36430bbb1c05f316197c16223a9e49223
Joel Silva <joeltadeu@gmail.com>
2017-01-22 15:51:45 -0200

    Add the first controller

--- src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
+++ src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Devopsbuddy1Application {
+public class DevopsbuddyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Devopsbuddy1Application.class, args);
+		SpringApplication.run(DevopsbuddyApplication.class, args);
 	}
 }
