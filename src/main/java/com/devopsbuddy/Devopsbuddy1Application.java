e1944e715932f333881ea39320e56d125995e994
Joel Silva <joeltadeu@gmail.com>
2017-01-22 15:26:45 -0200

    Initial commit for our first Spring boot project

---
+++ src/main/java/com/devopsbuddy/Devopsbuddy1Application.java
@@ -0,0 +1,12 @@
+package com.devopsbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devopsbuddy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devopsbuddy1Application.class, args);
+	}
+}
